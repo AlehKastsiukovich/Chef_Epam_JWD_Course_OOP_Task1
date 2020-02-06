d0c13c2ff9c35a0c1461956266fc9eac0106d800
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-06 23:09:12 +0300

    Add class SaladSpecificationSortById

--- src/main/java/by/javatraining/chef/repository/specification/SaladSpecificationSortById.java
+++ src/main/java/by/javatraining/chef/repository/specification/SaladSpecificationSortById.java
@@ -1,6 +1,21 @@
 package by.javatraining.chef.repository.specification;
 
-public class SaladSpecificationSortById {
+import by.javatraining.chef.comparator.VegetableIdComparator;
+import by.javatraining.chef.entity.Vegetable;
+import by.javatraining.chef.repository.VegetableRepository;
+import by.javatraining.chef.repository.VegetableRepositoryImpl;
 
+import java.util.Set;
+import java.util.TreeSet;
 
+public class SaladSpecificationSortById implements SaladSpecification{
+
+    @Override
+    public Set<Vegetable> query() {
+        VegetableRepository repository = VegetableRepositoryImpl.getInstance();
+        Set<Vegetable> sortedByIdSet = new TreeSet<Vegetable>(new VegetableIdComparator());
+        sortedByIdSet.addAll(repository.getAll());
+
+        return sortedByIdSet;
+    }
 }
